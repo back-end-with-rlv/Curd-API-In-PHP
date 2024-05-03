d59586b73dc5cc19cfd4a640f25cc3c35416b6d8
CodeWithrlv <129599537+back-end-with-rlv@users.noreply.github.com>
2024-05-03 18:57:18 +0530

    Update api.sql

--- Api/api.sql
+++ Api/api.sql
@@ -11,19 +11,11 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `api`
 --
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `api_token`
 --
 
@@ -35,7 +27,6 @@ CREATE TABLE `api_token` (
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
 -- Dumping data for table `api_token`
 --
 
@@ -44,7 +35,6 @@ INSERT INTO `api_token` (`id`, `token`, `hit_limit`, `hit_count`, `status`) VALU
 
 -- --------------------------------------------------------
 
---
 -- Table structure for table `users`
 --
 
@@ -54,47 +44,36 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`) VALUES
-(7, 'ratanl Lal   ', 'ratanlalver@gmail.com'),
-(9, 'Sunil kumar ', 'sunilverma@gmail.com');
+(7, 'ratanl Lal', 'ratanlalver@gmail.com'),
+(9, 'Sunil kumar', 'sunilverma@gmail.com');
 
---
 -- Indexes for dumped tables
 --
 
---
 -- Indexes for table `api_token`
 --
 ALTER TABLE `api_token`
   ADD PRIMARY KEY (`id`);
 
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
 -- AUTO_INCREMENT for dumped tables
 --
 
---
 -- AUTO_INCREMENT for table `api_token`
 --
 ALTER TABLE `api_token`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
