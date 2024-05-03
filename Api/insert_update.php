6e6817cfc9c5d3b29a4ef6d62fc64e70a9a332f8
CodeWithrlv <129599537+back-end-with-rlv@users.noreply.github.com>
2024-05-03 18:40:37 +0530

    Update insert_update.php

--- Api/insert_update.php
+++ Api/insert_update.php
@@ -1,1 +1,27 @@
+<?php
+include('token.php');
+if(!isset($status)){
+    if(isset($_POST['name']) && isset($_POST['email'])){
+        $name=mysqli_real_escape_string($con,$_POST['name']);
+        $email=mysqli_real_escape_string($con,$_POST['email']);
 
+        if(isset($_GET['id']) && $_GET['id']>0){
+            $id=mysqli_real_escape_string($con,$_GET['id']);
+            mysqli_query($con,"update users set name='$name', email='$email' where id='$id'");   
+            $data ="Data updated";
+            $codes ='10';
+        }else{
+            mysqli_query($con,"insert into users(name,email) values('$name','$email')");
+            $data ="Data inserted";
+            $codes ='8';
+        }
+
+        $status ='true';
+    }else{
+        $status ='true';
+        $data ="Provide valid column count";
+        $codes ='9';
+    }
+}
+echo json_encode(["status"=>$status,"data"=>$data,"codes"=>$codes]);
+?>
