943da81bcdd2cc1e1046ed54a780f83a582ab402
CodeWithrlv <129599537+back-end-with-rlv@users.noreply.github.com>
2024-05-03 17:52:04 +0530

    Update delete.php

--- Api/delete.php
+++ Api/delete.php
@@ -1,1 +1,18 @@
+<?php 
+include('token.php');
 
+if(!isset($status)){
+  if(isset($_POST['id']) && $_POST['id'] > 0){
+    $id = mysqli_real_escape_string($con,$_POST['id']);
+    mysqli_query($con, "DELETE FROM  users WHERE id = '$id' ");
+
+    $status = 'true';
+    $data = 'Data Deleted';
+    $codes = '7';
+  }else{
+    $status = 'true';
+    $data = 'Provide id ';
+    $codes = '6';
+  }
+}
+echo json_encode(['status'=>$status, 'data'=>$data, 'codes'=>$codes]);
