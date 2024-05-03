326c519de31446ccc13eaa458081a07fb6e8769b
CodeWithrlv <129599537+back-end-with-rlv@users.noreply.github.com>
2024-05-03 18:41:06 +0530

    Update insert_update.php

--- Api/insert_update.php
+++ Api/insert_update.php
@@ -4,7 +4,7 @@ if(!isset($status)){
     if(isset($_POST['name']) && isset($_POST['email'])){
         $name=mysqli_real_escape_string($con,$_POST['name']);
         $email=mysqli_real_escape_string($con,$_POST['email']);
-
+ 
         if(isset($_GET['id']) && $_GET['id']>0){
             $id=mysqli_real_escape_string($con,$_GET['id']);
             mysqli_query($con,"update users set name='$name', email='$email' where id='$id'");   
